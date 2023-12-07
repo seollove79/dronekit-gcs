f3aea0e5be46c638974345366b1ea54315581521
seollove79 <seollove@gmail.com>
2023-12-07 19:08:07 +0900

    LocationGlobal import

--- companion.py
+++ companion.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException, LocationGlobal
 import time
 import math
 
