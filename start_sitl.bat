28ce2fc5ea90b5eca98f8830e3193f2481537e28
seollove79 <seollove@gmail.com>
2024-02-08 11:09:27 +0900

    배치파일 추가

---
+++ start_sitl.bat
@@ -0,0 +1,5 @@
+@echo off
+start call dronekit-sitl copter --instance 0 --home=37.520995,126.621259,0,0
+start call dronekit-sitl copter --instance 1 --home=37.520995,126.621259,0,0
+start call dronekit-sitl copter --instance 2 --home=37.520995,126.621259,0,0
+pause
